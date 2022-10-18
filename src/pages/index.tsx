5129ee90aa0ce94437820f761d3224334390e055
Маклачков Александр <aleksandr.maklachkov@gazprombank.ru>
2022-10-18 21:44:31 +0300

    CSS reset and normilize

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import styles from '../styles/Home.module.scss'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </ul>
+      <img src="https://images.unsplash.com/photo-1495360010541-f48722b34f7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=436&q=80" />
+    </div>
+  )
+}
+
+export default Home
