1fe71c4c42e1756fb631743466b91ecca9574a24
Маклачков Александр <aleksandr.maklachkov@gazprombank.ru>
2022-10-18 20:19:08 +0300

    Sorting import members and object keys

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import Link from 'next/link'
-import Image from 'next/image'
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 
 import styles from '../styles/Home.module.scss'
 
