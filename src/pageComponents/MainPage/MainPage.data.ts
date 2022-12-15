86e109d673838cbb0b53e10c56cb1e8e3c6b0663
Маклачков Александр <aleksandr.maklachkov@gazprombank.ru>
2022-12-15 13:45:32 +0300

    Added stubs images

--- src/pageComponents/MainPage/MainPage.data.ts
+++ src/pageComponents/MainPage/MainPage.data.ts
@@ -3,34 +3,34 @@ import { IPhoto } from '@types'
 export const mainPagePhotos: IPhoto[] = [
   {
     id: 0,
-    link: '/images/main-page-slider/0.jpg',
+    link: '/images/main-page-slider/0.avif',
   },
   {
     id: 1,
-    link: '/images/main-page-slider/1.jpg',
+    link: '/images/main-page-slider/1.avif',
   },
   {
     id: 2,
-    link: '/images/main-page-slider/2.jpg',
+    link: '/images/main-page-slider/2.avif',
   },
   {
     id: 3,
-    link: '/images/main-page-slider/3.jpg',
+    link: '/images/main-page-slider/3.avif',
   },
   {
     id: 4,
-    link: '/images/main-page-slider/4.jpg',
+    link: '/images/main-page-slider/4.avif',
   },
   {
     id: 5,
-    link: '/images/main-page-slider/5.jpg',
+    link: '/images/main-page-slider/5.avif',
   },
   {
     id: 6,
-    link: '/images/main-page-slider/6.jpg',
+    link: '/images/main-page-slider/6.avif',
   },
   {
     id: 7,
-    link: '/images/main-page-slider/7.jpg',
+    link: '/images/main-page-slider/7.avif',
   },
 ]
